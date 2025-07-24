84b4e38f93944b3d5d123911fac316d0ee2d0e53
김아라 <ctlia100@gmail.com>
2025-07-24 19:42:35 +0900

    0
    플레이어 이동 구현
    낚시 모드 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //¿Ãµø
+    Vector3 dir = Vector3.zero;
+    [SerializeField] float speed;
+
+    //≥¨Ω√
+    bool isNowFishing = false;
+    bool isFishingZone = false;
+    [SerializeField] GameObject fishingRod;
+
+    //æ÷¥œ∏ﬁ¿Ãº«
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] AnimationClip[] ani;
+    SpriteRenderer sr;
+   
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!isNowFishing)
+        {
+            PlayerMove();
+
+            PlayerLook();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isFishingZone && !isNowFishing) 
+        {
+            Debug.Log("≥¨Ω√ Ω√¿€");
+            fishingRod.SetActive(true);
+            isNowFishing = true;
+            sr.sprite = sprites[4];
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isFishingZone && isNowFishing)
+        {
+            Debug.Log("≥¨Ω√ ¡æ∑·");
+            fishingRod.SetActive(false);
+            isNowFishing = false;
+            sr.sprite = sprites[0];
+        }
+    }
+
+    void PlayerMove()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        this.transform.position += dir * speed * Time.deltaTime;
+    }
+
+    void PlayerLook()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) //¿ß
+        {
+            sr.sprite = sprites[3];
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) //æ∆∑°
+        {
+            sr.sprite = sprites[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) //ø¿∏•
+        {
+            sr.sprite = sprites[2];
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) //øﬁ
+        {
+            sr.sprite = sprites[1];
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CanFishing"))
+        {
+            Debug.Log("≥¨Ω√ ∞°¥…");
+            isFishingZone = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CanFishing"))
+        {
+            Debug.Log("≥¨Ω√ ∫“∞°¥…");
+            isFishingZone = false;
+        }
+    }
+}
